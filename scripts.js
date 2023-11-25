bb8bac17a09539fff2d5b3afc089945dd4ac4abf
Andre <AVWildberger@gmail.com>
2023-11-25 15:55:11 +0100

    Adds super-fancy animations for hamburger bar

--- scripts.js
+++ scripts.js
@@ -1,19 +1,31 @@
-function toggleLinks() {
+function toggleLinks()
+{
     var links = document.getElementById("links");
     var text = document.getElementById("name");
 
-    if (links.style.display == "block")
-    {
-        links.style.display = "none";
+    function animationEndHandler() {
+        links.classList.remove("slide-out");
         text.style.display = "block";
-    } 
+        links.style.display = "none";
+
+        links.removeEventListener("animationend", animationEndHandler);
+    }
+
+    if (links.style.display == "block") {
+        links.addEventListener("animationend", animationEndHandler)
+
+        links.classList.add("slide-out");
+    }
     else
     {
         links.style.display = "block";
+        links.classList.add("slide-in");
 
-        if (screen.width < 2/3 * screen.height || screen.height > 3/2 * screen.width)
+        if (screen.width < (2/3) * screen.height || screen.height > (3/2) * screen.width)
         {
             text.style.display = "none";
         }
+
+        links.classList.remove("slide-out");
     }
-  }
+}
